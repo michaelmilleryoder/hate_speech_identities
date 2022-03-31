8ef462fcd0ccf9216c4679830b0d3b4b4e21d0a7
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2022-03-31 16:05:14 -0400

    stuff

--- code/data.py
+++ code/data.py
@@ -3,14 +3,11 @@
 class Dataset:
     """ Superclass for storing data and attributes of datasets """
 
-    def __init__(self, name, fpaths=None):
+    def __init__(self, name, load_args=None):
+        """ Args:
+                name: dataset name
+                load_args: list of arguments for loading the datasets (like file paths)
+        """
         self.name = name
-        self.fpaths = fpaths
-
-    def load(self):
-        """ To be overriden by subclasses """
-        pass
-
-class Kennedy2020Dataset(Dataset):
-
-    def load(self):
+        self.load_args = load_args
+        self.data = None
--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 """ Main entry point to run experiments:
         1. Load, process datasets
-        2. Test whether or not including hate toward hegemonic affects hate speech classification performance
-            2.1 Split data into with-hegemonic and no-hegemonic as well as with-control and no-control
+        2. Test whether ncluding hate toward hegemonic affects hate speech classification performance
+            2.1 Split data into with-hegemonic and no-hegemonic, with-control and no-control
             2.2 Run logistic regression classifiers on these splits, save out results
         3. Visualize a PCA of what identity splits contain similar hate (prediction-wise)
             3.1 Create datasets of hate speech targeting specific identities
@@ -10,25 +10,27 @@
 """
 
 from load_data import DataLoader
-from dataset import Dataset
+from data import Dataset
+
 
 def main():
-    
+    """ Run experiments """
+
     # Load datasets (if I modify this much, it should come from a config file or command line argument)
     datasets = [
-        Kennedy2020Dataset('kennedy2020', fpaths=['ucberkeley-dlab/measuring-hate-speech', 'binary']),
-        ElSherief2021Dataset('elsherief2021', fpaths=[
+        #Dataset('kennedy2020', ['ucberkeley-dlab/measuring-hate-speech', 'binary']),
+        Dataset('elsherief2021', load_args=[
             '/storage2/mamille3/data/hate_speech/elsherief2021/implicit_hate_v1_stg3_posts.tsv',        
             '/storage2/mamille3/data/hate_speech/elsherief2021/implicit_hate_v1_stg1_posts.tsv',
-        ], pandas_args='sep="\t"')
-        Dataset('salminen2018', fpaths=['/storage2/mamille3/data/hate_speech/salminen2018/salminen2018.csv'], 
-            pandas_args="index_col=0"),
-        Dataset('sbic', fpaths=[
-            '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.trn.csv',
-            '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.dev.csv',
-            '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.tst.csv',
-        ], pandas_args="index_col=0"),
-        Dataset('cad', fpaths=['/storage2/mamille3/data/hate_speech/cad/cad_v1_1.tsv']),
+        ])
+        #Dataset('salminen2018', fpaths=['/storage2/mamille3/data/hate_speech/salminen2018/salminen2018.csv'], 
+        #    pandas_args="index_col=0"),
+        #Dataset('sbic', fpaths=[
+        #    '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.trn.csv',
+        #    '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.dev.csv',
+        #    '/storage2/mamille3/data/hate_speech/sbic/SBIC.v2.agg.tst.csv',
+        #], pandas_args="index_col=0"),
+        Dataset('cad', load_args=['/storage2/mamille3/data/hate_speech/cad/cad_v1_1.tsv']),
     ]
 
 
