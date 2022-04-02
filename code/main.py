fcf6d011a5656be77f14337eb16a7ab52ed34af7
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2022-04-02 13:25:27 -0400

    control terms work

--- code/main.py
+++ code/main.py
@@ -32,12 +32,11 @@ class Experiment:
     def run(self):
         """ Run experiment """
 
-        self.load_datasets(datasets)
-        process_datasets(datasets, loaders)
-
+        self.load_datasets()
+        self.process_datasets()
         if calculate_control:
-            get_control_terms(datasets)
-
+            self.get_control_terms()
+        self.label_control()
         self.save_datasets()
         
     def load_datasets(self):
@@ -52,6 +51,12 @@ class Experiment:
         """ Process datasets """
         for dataset, loader in zip(self.datasets, self.loaders):
             loader.process(dataset)
+    
+    def label_control(self):
+        """ Label control instances in datasets. This is separate from processing since
+            calculating control terms depends on other processing. """
+        for dataset, loader in zip(self.datasets, self.loaders):
+            loader.label_control(dataset)
 
     def save_datasets(self):
         """ Save processsed datasets """
@@ -84,145 +89,27 @@ class Experiment:
         marg_counts = marg_targets.drop(columns=['group_label']).pivot_table(index=['group'], columns=['dataset'])
         log_marg_counts = marg_counts.apply(np.log2).replace(-np.inf, -1)
         marg = log_marg_counts.copy()
-
-        # TODO: edit here
         control_terms = []
-        
         for heg_term, heg_vec in log_heg_counts.iterrows():
             distances = np.linalg.norm(marg.values - heg_vec.values, axis=1)
             closest_marg = marg.index[np.argmin(distances)]
             control_terms.append(closest_marg)
             marg.drop(closest_marg, inplace=True) 
-            
-        display(log_marg_counts.loc[control_terms])
-        display(log_heg_counts)
-        
+
         # Save control terms out
-        control_terms
-        
-        
-        # In[25]:
-        
-        
         outpath = '/storage2/mamille3/hegemonic_hate/control_identity_terms.txt'
         with open(outpath, 'w') as f:
             for term in control_terms:
                 f.write(f'{term}\n')
         
-        
-        # In[36]:
-        
-        
         # Check counts across datasets for heg and control
+        print("Heg counts compared with control counts")
         print(heg_counts.sum())
         print(heg_counts.sum().sum())
         print()
         print(marg_counts.loc[control_terms].sum())
         print(marg_counts.loc[control_terms].sum().sum())
         
-        
-        # In[44]:
-        
-        
-        # Create boolean column of control/not
-        for dataset in hate_datasets:
-            print(dataset)
-            hate_datasets[dataset]['in_control'] = hate_datasets[dataset]['target_groups'].map(lambda x: any([groups_norm.get(term, term) in control_terms for term in x]) if isinstance(x, list) else False)
-            print(hate_datasets[dataset].in_control.sum())
-        
-        
-        # In[129]:
-        
-        
-        # Check hegemonic instance counts (not sure why this isn't as close, but something to do with basing the matching off of term counts vs this is instances with any of the terms)
-        for dataset in hate_datasets:
-            print(dataset)
-            print(sum(hate_datasets[dataset]['group_label']=='hegemonic'))
-        
-        
-        # In[78]:
-        
-        
-        # These might be more popular than the hegemonic ones, though, in which case it would create dataset splits that are more distinct
-        # Want them to be roughly as popular overall/for each dataset as the hegemonic set of labels is
-        distros
-        
-        target_dataset_counts['group_normalized'] = target_dataset_counts.group.map(lambda x: groups_norm.get(x, x))
-        target_dataset_counts['control_group'] = target_dataset_counts.group_normalized.isin(control_groups)
-        control_group_instances = target_dataset_counts.query('control_group').groupby('dataset')['count'].sum()
-        distros.join(control_group_instances)[['count_hegemonic', 'count']]
-        
-        
-        # In[79]:
-        
-        
-        # Look for marginalized identities that have similar frequency distributions to hegemonic terms
-        # Just get Euclidean distance between vectors of target groups (with maybe a log somewhere since kennedy2020 is so different?)
-        # Try individual matching between hegemonic terms and marginalized
-        
-        
-        # ## Sample control marginalized identities weighted by popularity
-        
-        # In[60]:
-        
-        
-        # Normalize counts by dataset length
-        distros = pd.DataFrame(group_label_distros, columns=['corpus', 'total_items', 'items_labeled_with_target', 'count_hegemonic', 'count_marginalized', 'count_other']).set_index('corpus')
-        distros
-        
-        merged = target_dataset_counts.join(distros, on=['dataset']).drop(columns=['count_hegemonic', 'count_marginalized', 'count_other'])
-        print(len(merged))
-        print(len(target_dataset_counts))
-        merged.head()
-        
-        merged['count_normalized'] = merged['count']/merged['items_labeled_with_target']
-        merged.head()
-        
-        # Group most popular identities first
-        gped = merged.groupby('group').agg({'count_normalized': 'sum', 'group_label': 'first'}).sort_values('count_normalized', ascending=False)
-        gped
-        
-        gped['group_normalized'] = gped.index.map(lambda x: groups_norm.get(x, x))
-        
-        def assign_label(labels):
-            label = 'other'
-            labels = set(labels)
-            if 'marginalized' in labels and not 'hegemonic' in labels:
-                label = 'marginalized'
-            elif 'hegemonic' in labels:
-                label = 'hegemonic'
-            return label
-        
-        gped_norm = gped.groupby('group_normalized').agg({'count_normalized': 'sum', 'group_label': assign_label}).sort_values('count_normalized', ascending=False)
-        gped_norm
-        
-        # Sample a group of identities for the control group
-        possibilities = gped_norm.query('group_label == "marginalized"').iloc[:100]
-        possibilities
-        
-        
-        # In[77]:
-        
-        
-        control_groups = possibilities.sample(2, weights=possibilities.count_normalized, random_state=9).index
-        control_groups
-        
-        
-        # In[78]:
-        
-        
-        # These might be more popular than the hegemonic ones, though, in which case it would create dataset splits that are more distinct
-        # Want them to be roughly as popular overall/for each dataset as the hegemonic set of labels is
-        distros
-        
-        target_dataset_counts['group_normalized'] = target_dataset_counts.group.map(lambda x: groups_norm.get(x, x))
-        target_dataset_counts['control_group'] = target_dataset_counts.group_normalized.isin(control_groups)
-        control_group_instances = target_dataset_counts.query('control_group').groupby('dataset')['count'].sum()
-        distros.join(control_group_instances)[['count_hegemonic', 'count']]
-        
-        
-                
-
 
 def main():
     """ Run experiments """
